f3a012490111975e910aecf12d85c71b065e5e13
AzulCaballero <xazulcaballero@gmail.com>
2020-05-07 23:24:30 -0500

    update

--- main-scrapper.py
+++ main-scrapper.py
@@ -1,9 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
-import csv
-# import pandas as pd
+import pandas as pd
 
-# Process 1: Fetch raw HTML from a URL
+# Process 1: Fetch game page URLs from game center
 gc_url = 'http://cfbhc.com/index.php?/forum/575-game-center/page/1/'
 gc_page = requests.get(gc_url).text
 gc_soup = BeautifulSoup(gc_page, 'lxml')
@@ -14,9 +13,10 @@ for threads in gc_soup.find_all('a', attrs={'data-ipshover-timeout': '1.5'}):
    gamelist.append(rows)
 
 del gamelist[0]
-print(gamelist)
 
-# Process 1: Fetch raw HTML from a URL
+# Process 2: Open data spreadsheet
+
+# Process 3: Let's stugglebus through some games for raw data
 raw = []
 
 for game in gamelist:
@@ -25,23 +25,20 @@ for game in gamelist:
 
     row = []
     tables = soup.find_all('table', attrs={'class': 'boxscore'})
+
     for table in tables:
         boxes = table.find_all('tr')
 
         for box in boxes:
             cols = box.find_all('td')
             cols = [ele.text.strip() for ele in cols]
+            if len(cols) > 0:
+                x = cols[0]
             row.append([ele for ele in cols if ele])
-            raw.append(row)
-            row = []
 
 raw = [i for i in raw if i[0]!='']
+dataset = pd.DataFrame(row)
 
+dataset.to_csv("Dataset.csv", index = False)
 
-# open csv
-csvfile = open('cbbtest.csv', 'w')
-writer = csv.writer(csvfile)
-writer.writerow(raw)
-
-# close csv
-csvfile.close()
+print("fuck ya")
